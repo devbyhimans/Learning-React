f74cf27f3cc8f79e31d20fe75539881dbf296539
Himanshu <himanshu620434@gmail.com>
2025-07-25 16:52:24 +0530

    New Method of React Router

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -4,26 +4,27 @@ import Home from './Pages/Home/Home'
 import Products from './Pages/Products/Products'
 import About from './Pages/About/About'
 import Contact from './Pages/Contact/Contact'
-import {Routes ,Route} from 'react-router-dom'
-
+import {Route, createBrowserRouter, RouterProvider,createRoutesFromElements} from 'react-router-dom'
+import RootLayout from './Layout/RootLayout'
 
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      <div className="container">
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      
-      </div>
 
-    </div>
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='products' element={<Products />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
+
+
+  return (
+      <RouterProvider router={router}/>
   )
 }
 
